ca41744ce8b6fbc1772d424a13db128870c4ffe8
Ruslan <rusik.sorok1337@gmail.com>
2023-12-16 14:51:21 +0200

    added custom hook

--- App.js
+++ App.js
@@ -1,5 +1,32 @@
+import { Text, View } from "react-native";
 import Navigator from "./src/navigation";
+import useAppStateListener from "./src/utils/hooks";
+import { Dimensions, Image } from "react-native";
+const { width, height } = Dimensions.get("screen");
 
 export default function App() {
-    return <Navigator />;
+    const { appState } = useAppStateListener();
+
+    return (
+        <>
+            {appState !== "active" && (
+                <View
+                    style={{
+                        flex: 2000,
+                    }}
+                >
+                    <Image
+                        style={{
+                            width: width,
+                            height: height,
+                        }}
+                        source={{
+                            uri: "https://img.freepik.com/free-photo/top-view-pepperoni-pizza-with-mushroom-sausages-bell-pepper-olive-corn-black-wooden_141793-2158.jpg",
+                        }}
+                    />
+                </View>
+            )}
+            <Navigator />
+        </>
+    );
 }
